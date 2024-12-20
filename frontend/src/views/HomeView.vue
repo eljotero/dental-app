96067fd3a513f21d2e2dacc09bdcce50e1630688
Aleksander Janicki <aleksander.janicki002@gmail.com>
2024-12-20 20:07:52 +0100

    feat: added ts and shadcn

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+import {Button} from '../components/ui/button';
+</script>
+
+<template>
+  <main>
+    <TheWelcome/>
+    <Button>Click me</Button>
+  </main>
+</template>
